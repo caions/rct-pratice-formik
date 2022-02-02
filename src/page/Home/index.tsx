edd8d8f73d3f4d3851b4e79432aa519dd14649d1
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-02-02 07:37:14 -0300

    feat: initial formik

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,13 +1,37 @@
-import { Button } from '../../components/Buttom';
+import { Field, Form, Formik, FormikHelpers, FormikValues } from 'formik';
 import styles from './style.module.css';
 
+interface IFields {
+  nome: string;
+  email: string;
+}
+
 export const Home: React.FC = () => {
+  // eslint-disable-next-line prefer-const
+
+  const initialValues = {
+    nome: '',
+    email: '',
+  };
+
+  const handleSubmit = (
+    values: IFields,
+    formikHelpers: FormikHelpers<IFields>,
+  ) => {
+    alert(JSON.stringify(values));
+  };
+
   return (
-    <>
-      <div className={styles.container}>
-        <h1>Tela Home</h1>
-        <Button />
-      </div>
-    </>
+    <div className={styles.container}>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {() => (
+          <Form>
+            <Field name="nome" />
+            <Field name="email" />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
