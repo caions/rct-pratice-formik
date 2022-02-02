4a712f03eabcd4ab8935314c59e8e8f3df4778b3
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-02-02 07:59:20 -0300

    feat: formik errors

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -14,22 +14,44 @@ export const Home: React.FC = () => {
     email: '',
   };
 
+  const validate = (values: IFields): IFields | undefined => {
+    const errors = {} as IFields;
+    if (!values.nome) {
+      errors.nome = 'Nome é obrigatório';
+    }
+
+    if (values.email.length < 2) {
+      errors.email = 'email inválido';
+    }
+    return errors;
+  };
+
   const handleSubmit = (
     values: IFields,
     formikHelpers: FormikHelpers<IFields>,
   ) => {
     alert(JSON.stringify(values));
+    //  console.log(formikHelpers);
   };
 
   return (
     <div className={styles.container}>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        {() => (
-          <Form>
-            <Field name="nome" />
-            <Field name="email" />
-            <button type="submit">Submit</button>
-          </Form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validate={validate}
+      >
+        {({ errors }) => (
+          <div className={styles.form}>
+            <Form>
+              <Field name="nome" />
+              {errors.nome ? <span>{errors.nome}</span> : ''}
+              <Field name="email" />
+              {errors.email ? <span>{errors.email}</span> : ''}
+              <br />
+              <button type="submit">Submit</button>
+            </Form>
+          </div>
         )}
       </Formik>
     </div>
